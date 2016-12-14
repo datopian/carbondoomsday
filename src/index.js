15eff87b61edd43a9ccc4d16b96984062301dd77
anuveyatsu <anuar.ustayev@gmail.com>
2016-12-14 10:54:32 +0600

    [dataSource][m]: added support for dataSource

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Chart from './components/lineChart.js';
+
+ReactDOM.render(
+  <Chart />,
+  document.getElementById('root')
+);
