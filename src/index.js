7f48df3048476055d4f8c763376880b8a02e923b
anuveyatsu <anuar.ustayev@gmail.com>
2017-01-06 13:57:47 +0000

    [react][xs]: made plotly chart as default - refs #41

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ ReactDOM.render(
   <PlotlyChart data={[{x: ['2014-01-02', '2014-01-03'], y: [5, 8], mode: "lines"}]} />,
   document.getElementById("root")
 );
-
+/*
 ReactDOM.render(
   <VegaLiteChart data={[{x: '2014-01-02', y: 5}, {x: '2014-01-03', y: 8}]} />,
   document.getElementById("root")
 );
-
+*/
 ReactDOM.render(
   <HandsOnTable data={[[1,2],[3,4],[5,6]]} colHeaders={["f","s"]} />,
   document.getElementsByClassName("resource-info")[0]
