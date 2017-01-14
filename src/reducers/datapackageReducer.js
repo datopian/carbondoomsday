25274b1fba41d7f0f1d2f21fad9e2c8e5b59b244
anuveyatsu <anuar.ustayev@gmail.com>
2017-01-14 16:55:12 +0600

    [redux][xs]: fixed redux reducer - refs #43

--- src/reducers/datapackageReducer.js
+++ src/reducers/datapackageReducer.js
@@ -1,14 +1,16 @@
 import { combineReducers } from 'redux'
 import * as actionTypes from '../constants/actionTypes'
 
-export default function update(state = {
-  datapackage: [],
+const initialState = {
+  datapackage: {},
   resources: []
-}, action) {
+}
+
+export default function update(state = initialState, action) {
   switch (action.type) {
     case actionTypes.RECEIVE_DATAPACKAGE:
       return Object.assign({}, state, {
-        datapackage: action.datapackage
+        datapackage: action.dp
       })
     case actionTypes.RECEIVE_RESOURCE:
       return Object.assign({}, state, state.resources.push(action.resources))
