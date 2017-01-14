cc368b791fca4115554e7a035a24d54e7d999478
anuveyatsu <anuar.ustayev@gmail.com>
2017-01-14 13:50:09 +0600

    [datapackage][xs]: redone getDataPackage method - refs #42

--- src/js/getDataPackage.js
+++ src/js/getDataPackage.js
@@ -1,11 +1,5 @@
 const Datapackage = require('datapackage-test').Datapackage
 
-async function getDataPackage(url) {
-  //return new Datapackage(url).then(dp => dp)
-  const dp = await new Datapackage(url)
-  const table = await dp.resources[0].table
-  const data = await table.read()
-  return data
+export default function getDataPackage(url) {
+  return new Datapackage(url)
 }
-
-export default getDataPackage
