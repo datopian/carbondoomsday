842b4410b2bda24138af26f60087865c796183bb
anuveyatsu <anuar.ustayev@gmail.com>
2017-01-14 16:53:08 +0600

    [redux][s]: added resource loader - refs #43

--- src/actions/datapackageActions.js
+++ src/actions/datapackageActions.js
@@ -1,4 +1,7 @@
+import fetch from 'isomorphic-fetch';
 import * as actionTypes from "../constants/actionTypes"
+import Papa from "papaparse"
+const Datapackage = require('datapackage-test').Datapackage
 
 export function receiveDatapackage(dp) {
   return {
@@ -14,19 +17,21 @@ export function receiveResource(resources) {
   }
 }
 
-export async function getDataPackage(url) {
+export function getDataPackage(url) {
   return dispatch => {
-    const dp = await new Datapackage(url)
-    dispatch(receiveDatapackage(dp.descriptor))
+    return new Datapackage(url).then(dp => {
+        dispatch(receiveDatapackage(dp.descriptor))
+      })
   }
 }
 
-/*
 export function fetchResource(csvUrl) {
   return dispatch => {
     return fetch(csvUrl)
       .then(response => response.text())
-      .then(result => dispatch(receiveResource(result)))
+      .then(result => {
+        result = Papa.parse(result)
+        dispatch(receiveResource(result.data))
+      })
   }
 }
-*/
