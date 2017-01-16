9e7d0f507472965d73d994883a02fc767bec89ef
anuveyatsu <anuar.ustayev@gmail.com>
2017-01-17 03:10:11 +0600

    [containers][m]: added DataPackageViewContainer - refs #47

---
+++ src/containers/DataPackageViewContainer.js
@@ -0,0 +1,64 @@
+import React from "react"
+import PlotlyChart from "../components/plotly.js"
+
+class DataPackageViewContainer extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: [],
+      layout: []
+    }
+
+    //dispatch redux actions
+  }
+
+  generateSpec(views) {
+    let plotlySpecs = []
+    views.forEach((view) => {
+      let plotlySpec = {
+        "layout": {
+          "xaxis": {
+            "title": view.state.group
+          }
+        }
+      }
+      plotlySpecs.push(plotlySpec)
+    })
+    return plotlySpecs
+  }
+
+  convertData(data, dp) {
+    let dataset = []
+    let series = dp.views[0].state.series
+    for (let i = 0; i < series.length; i++) {
+      dataset.push({x: [], y: [], mode: "lines"})
+      dataset[i].x = data.map((row) =>
+        row[0]
+      )
+
+      dataset[i].y = data.map((row) =>
+        row[4]
+      )
+    }
+    return dataset
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let data = this.convertData(nextProps.resources[0], nextProps.datapackage)
+    let layout = this.generateSpec(nextProps.datapackage.views)
+    this.setState({
+      data: data,
+      layout: layout
+    })
+  }
+
+  render() {
+    return (
+      <PlotlyChart data={this.state.data} layout={this.state.layout} />
+    )
+  }
+}
+
+export default DataPackageViewContainer
