f637673c66b48cbb5603999eeea43fccea8c5972
subhankar <subhankar.biswas@target.com>
2017-01-19 23:56:53 +0530

    [fix][s] fix indentations - ref #54

--- src/components/PackageListItem.js
+++ src/components/PackageListItem.js
@@ -2,31 +2,31 @@ import React, {PropTypes} from 'react';
 
 class PackageListItem extends React.Component {
 
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+  }
 
 	render(){
-		const {name, description, packageUrl} = this.props;
-		return (
-			<div className="panel panel-default">
-				<div className="panel-heading">
-					<h3 className="panel-title pull-left">{name}</h3>
-					<button className="btn btn-success pull-right">
-						<a href={packageUrl}>Explore >></a>
-					</button>
-					<div className="clearfix"></div>
-				</div>
-				<div className="panel-body">{description}</div>
-			</div>
-		);
-	}
+	  const {name, description, packageUrl} = this.props;
+	  return (
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <h3 className="panel-title pull-left">{name}</h3>
+          <button className="btn btn-success pull-right">
+            <a href={packageUrl}>Explore >></a>
+          </button>
+          <div className="clearfix"></div>
+        </div>
+        <div className="panel-body">{description}</div>
+      </div>
+    );
+  }
 }
 
 PackageListItem.propTypes = {
-	name: PropTypes.string.isRequired,
-	description: PropTypes.string,
-	packageUrl: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  packageUrl: PropTypes.string.isRequired
 };
 
 export default PackageListItem;
