cd18b56a747dcc17ad27773b11020f644403cb3e
anuveyatsu <anuar.ustayev@gmail.com>
2017-01-19 20:29:31 +0600

    [redux][s]: moved redux dispatch to index.js - refs #49

--- src/index.js
+++ src/index.js
@@ -8,17 +8,25 @@ import TabularResourceViewContainer from "./containers/TabularResourceViewContai
 //redux:
 import { Provider } from 'react-redux'
 import configureStore from './store/configureStore'
+import * as actions from './actions/datapackageActions'
 
 const store = configureStore(
   window.devToolsExtension && window.devToolsExtension()
 )
 
-ReactDOM.render(
-  <Provider store={store}>
-    <DataPackageViewContainer />
-  </Provider>,
-  document.getElementById("root")
-);
+store.dispatch(actions.getDataPackage(DataPackageJsonUrl))
+
+let divForChart = document.getElementsByClassName("vis")
+
+for(let i=0; i<divForChart.length; i++) {
+  ReactDOM.render(
+    <Provider store={store}>
+      <DataPackageViewContainer idx={i} />
+    </Provider>,
+    document.getElementsByClassName("vis")[i]
+  );
+}
+
 /*
 ReactDOM.render(
   <VegaLiteChart data={[{x: '2014-01-02', y: 5}, {x: '2014-01-03', y: 8}]} />,
