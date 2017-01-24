a08af1f94d7ae3d4c514e4764e3b0bc8829214e2
anuveyatsu <anuar.ustayev@gmail.com>
2017-01-24 19:10:36 +0600

    [tests][s]: refactored tests for presentational components - refs #72

--- __tests__/src/components/plotly.test.js
+++ __tests__/src/components/plotly.test.js
@@ -1,15 +1,29 @@
 import React from "react"
-import { shallow, mount } from 'enzyme'
-import sinon from 'sinon'
+import { mount } from 'enzyme'
 import PlotlyChart from "../../../src/components/plotly.js"
+
+const mockData = [{
+  x: [
+    '2014-01-01T18:00:00.000Z',
+    '2014-01-02T18:00:00.000Z',
+    '2014-01-05T18:00:00.000Z'
+  ],
+  y: [ 14.23, 13.76, 13.55 ],
+  mode: 'lines',
+  name: 'DEMOClose'
+}]
+
+const mockLayout = { layout: { xaxis: { title: 'Date' } } }
 
 describe("plotly chart module", () => {
 
-  it("should have generated plotly graph", () => {
-    sinon.spy(PlotlyChart.prototype, 'componentDidMount')
-    const wrapper = mount(<PlotlyChart data={[{x: ['2014-01-02', '2014-01-03'], y: [5, 8], mode: "lines"}]} />)
-
-    expect(wrapper.contains(<svg className="main svg" />)).to.equal(true)
+  it("should receive correct props and render div with specific id", () => {
+    let idx = 0
+    const wrapper = mount(<PlotlyChart data={mockData} layout={mockLayout} idx={idx} />)
+    expect(wrapper.html()).toEqual('<div id="plotly'+idx+'"></div>')
+    expect(wrapper.prop('idx')).toEqual(0)
+    expect(wrapper.prop('data')).toEqual(mockData)
+    expect(wrapper.prop('layout')).toEqual(mockLayout)
   })
 
 })
--- __tests__/src/components/vegaLite.test.js
+++ __tests__/src/components/vegaLite.test.js
@@ -3,14 +3,46 @@ import { shallow, mount } from 'enzyme'
 import sinon from 'sinon'
 import VegaLiteChart from "../../../src/components/vegaLite.js"
 
+const mockSpec = {
+  data: {
+    values: [
+      {
+        "Date": '2014-01-01T18:00:00.000Z',
+        "DEMOOpen": 14.32,
+        "DEMOHigh": 14.59,
+        "DEMOLow": 14,
+        "DEMOClose": 14.23
+      },
+      {
+        "Date": '2014-01-02T18:00:00.000Z',
+        "DEMOOpen": 14.06,
+        "DEMOHigh": 14.22,
+        "DEMOLow": 13.57,
+        "DEMOClose": 13.76
+      }
+    ]
+  },
+  layers: [
+    {
+      mark: 'line',
+      encoding: {
+        x: { field: 'Date', type: 'temporal' },
+        y: { field: 'DEMOClose', type: 'quantitative' }
+      }  
+    }
+  ]
+}
+
 describe("vegaLite chart module", () => {
 
-  it("should have generated vegaLite graph", () => {
-    sinon.spy(VegaLiteChart.prototype, 'componentDidMount')
-    const wrapper = mount(<VegaLiteChart data={[{x: '2014-01-02', y: 5}, {x: '2014-01-03', y: 8}]} />)
-
-    expect(wrapper.contains(<div className="vega" />)).to.equal(true)
-    expect(wrapper.contains(<canvas className="marks" />)).to.equal(true)
+  it("should receive correct props and render div with specific id", () => {
+    let idx = 0
+    const spy = sinon.spy(VegaLiteChart.prototype, 'componentDidMount')
+    const wrapper = mount(<VegaLiteChart vlSpec={mockSpec} idx={idx} />)
+    expect(spy.calledOnce).toEqual(true)
+    expect(wrapper.html()).toEqual('<div id="vega'+idx+'"></div>')
+    expect(wrapper.prop('idx')).toEqual(0)
+    expect(wrapper.prop('vlSpec')).toEqual(mockSpec)
   })
 
 })
