47509f0470b330e95d4b44e26e623f8c8cac6771
subhankar <subhankar.biswas@target.com>
2017-01-29 19:43:14 +0530

    [fix][s]: Typo in environment variable

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -34,7 +34,7 @@ export default {
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
     path: path.resolve(__dirname, SCRIPTS_PATH ),
-    publicPath: `${process.env.BIT_STORE_URL===undefined ? '' : process.env.BIT_STORE_UR}/static/scripts/`,
+    publicPath: `${process.env.BIT_STORE_URL===undefined ? '' : process.env.BIT_STORE_URL}/static/scripts/`,
     pathInfo: true,
     filename: '[name].[chunkhash].js'
   },
