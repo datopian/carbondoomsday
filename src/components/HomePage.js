a70b4288f4f7d1b8f7f01deafc8192b20b9a4fc6
anuveyatsu <anuar.ustayev@gmail.com>
2017-01-30 11:40:53 +0600

    [homepage][s]: styling for homepage using bootstrap4 - refs #96

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,12 +9,15 @@ class HomePage extends React.Component {
 
   render() {
     return (
-      <div >
-        <h1>Welcome to Data Package Registry</h1>
-        <Link to={"/core"}>GO TO Core DATA PAGE</Link>
+      <div>
+        <div className="d-flex justify-content-center mb-4">
+          <h1>Welcome to Data Package Registry</h1>
+        </div>
+        <div className="d-flex justify-content-center">
+          <Link to={"/core"} className="btn btn-secondary">Core Data Package</Link>
+        </div>
       </div>
     );
   }
 }
 export default HomePage;
-
