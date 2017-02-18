3b37ac708a264df44db7d4b4bb200c371f05ee83
subhankar <subhankar.biswas@target.com>
2017-02-18 16:07:54 +0530

    [s] Remove code obsoleted by reversion to MPA -refs #111

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
 import datapackage from './datapackageReducer'
-import publisherPage from './publisherPageReducer'
 
 const rootReducer = combineReducers({
-  routing: routerReducer
-  , dpr: datapackage
-  , publisherPage
+  dpr: datapackage
 })
 
 export default rootReducer
